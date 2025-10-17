ddd38f19a736bf972aa04ddbe3a2c305db75b57f
M.ALmustafa <mohalfade@gmail.com>
2025-10-17 19:14:46 +0300

    Add updated IAM service code from local copy

---
+++ source-code/Back-End/services/identity_and_access_management-copy/src/types/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @description A union type that represents fields used for auditing purposes within models.
+ *
+ * This type includes: `createdAt` `updatedAt`
+ */
+export type auditingFields = "createdAt" | "updatedAt";
---
+++ source-code/Back-End/services/identity_and_access_management-copy/src/utils/handleContollerErrors.ts
@@ -0,0 +1,24 @@
+import { Response } from "express";
+import { HttpError } from "../errors/httpError";
+import { createResponseObject } from "./createResponseObject";
+
+interface controllerError {
+  res: Response;
+  error: unknown;
+  defaultErrorMessage: string;
+}
+
+/**
+ * Handles errors that occur during the execution of a controller
+ * @param {Object} props - Error handling properties
+ * @param {string} props.defaultErrorMessage - The default error message to return if the error is not an instance of HttpError
+ * @param {Response} props.res - The Express Response object
+ * @param {unknown} props.error - The error that occurred during the execution of the controller
+ * @returns {Response} - The error response to send to the client
+ */
+export const handleControllerError = <T>({ defaultErrorMessage, res, error }: controllerError): Response => {
+  if (error instanceof HttpError) {
+    return res.status(error.statusCode || 500).json(<T>({ error: { message: defaultErrorMessage, details: error.message } }));
+  }
+  return res.status(500).json(<T>({ error: { message: defaultErrorMessage, details: "Internal server error" } }));
+};
